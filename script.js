22dec0b8a0e2c7b88bdb62e6d9f1b82af3a8ed64
MuHisham <172366361+MuHisham@users.noreply.github.com>
2024-06-18 20:50:24 +0400

    fix: Modified player function to return string

--- script.js
+++ script.js
@@ -4,13 +4,24 @@ let choices = ["rock", "paper", "scissor"];
 function getComputerChoice() {
     let randint = Math.floor(Math.random() * 3);
     return choices[randint];
-}
+};
 
 // Get player choice
 function getHumanChoice() {
-    console.log("Enter your choice (only the number)\n 1. Rock\n 2. Paper\n 3. Scissor");
-    let pChoice = prompt();
-    console.log(choices[pChoice-1]);
-}
+    console.log("Enter your choice \n(Rock | Paper | Scissor)");
+    while (true) {
+        let pChoice = prompt().toLowerCase();
+        if (choices.includes(pChoice)) {
+            return pChoice;
+        }
+    } 
+};
 
-getHumanChoice();
+console.log(getHumanChoice());
+
+// let humanScore = 0;
+// let computerScore = 0;
+
+// function playRound(humanChoice, computerChoice) {
+
+// }
