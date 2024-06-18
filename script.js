a5d76779e9db46060eb9f6c29db969ba2bf69da4
MuHisham <172366361+MuHisham@users.noreply.github.com>
2024-06-18 20:19:13 +0400

    feat: recieve player input

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+let choices = ["rock", "paper", "scissor"];
+
 // Get rock, paper, scissor randomly
 function getComputerChoice() {
-    let choices = ["rock", "paper", "scissor"];
     let randint = Math.floor(Math.random() * 3);
     return choices[randint];
 }
 
-console.log(getComputerChoice());
+// Get player choice
+function getHumanChoice() {
+    console.log("Enter your choice (only the number)\n 1. Rock\n 2. Paper\n 3. Scissor");
+    let pChoice = prompt();
+    console.log(choices[pChoice-1]);
+}
+
+getHumanChoice();
