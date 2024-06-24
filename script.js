86e22e1924324a2f7616be896baf2dc702d68285
MuHisham <172366361+MuHisham@users.noreply.github.com>
2024-06-24 21:05:54 +0400

    Feat: Added functionality and game logic

--- script.js
+++ script.js
@@ -1,42 +1,110 @@
+const playerButtons = document.querySelectorAll(".p-items");
+const playerIcons = document.querySelectorAll(".p-icon");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissor = document.querySelector("#scissor");
+const computerChoice = document.querySelector(".computer");
+let selectedBtn = null;
+let selectedIcon = null;
+let humanChoice = null;
+
+const cRock = document.querySelector(".c-rock");
+const cPaper = document.querySelector(".c-paper");
+const cScissor = document.querySelector(".c-scissor");
+
+let humanScore = document.querySelector(".player-score");
+let computerScore = document.querySelector(".computer-score");
+let humanCount = 0;
+let computerCount = 0;
+
+const resetBtnStyle = (btn) => {
+    btn.style.borderColor = 'black';
+}
+const resetBtnStyle2 = (btn) => {
+    btn.style.color = 'black';
+}
+
 let choices = ["rock", "paper", "scissor"];
 
-// Get rock, paper, scissor randomly
+// Get rock, paper, scissor randomly and setting the animation
 function getComputerChoice() {
     let randint = Math.floor(Math.random() * 3);
-    return choices[randint];
-};
-
-// Get player choice
-function getHumanChoice() {
-    console.log("Enter your choice \n(Rock | Paper | Scissor)");
-    while (true) {
-        let pChoice = prompt().toLowerCase();
-        if (choices.includes(pChoice)) {
-            return pChoice;
-        }
+    let computerOption =  choices[randint];
+    if (computerOption == "paper") {
+        cRock.style.order = 3;
+        cPaper.style.order = 1;
+        cScissor.style.order = 2;   
     } 
+    else if (computerOption == "scissor") {
+        cRock.style.order = 2;
+        cPaper.style.order = 3;
+        cScissor.style.order = 1;   
+    }
+    else {
+        cRock.style.order = 1;
+        cPaper.style.order = 2;
+        cScissor.style.order = 3;   
+    }
+    computerChoice.style.cssText = "animation: infiniteScroll 500ms linear 4;"
+    setTimeout(() => computerChoice.style.cssText = "animation: none;", 2000);
+    return computerOption;
 };
-
-let humanScore = 0;
-let computerScore = 0;
 
 // Game logic
 function playRound(humanChoice, computerChoice) {
-    let result = "";
     if((humanChoice === "rock" && computerChoice === "scissor") || 
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissor" && computerChoice === "paper")) {
-        result = `You win! ${humanChoice} beats ${computerChoice}`;
-        humanScore++;
+        humanCount++;
+        setTimeout(function () {
+            humanScore.innerText = `${humanCount}`;
+            computerScore.innerText = `${computerCount}`;
+        }, 2000)
     }
     else if (humanChoice === computerChoice) {
-        result = "Its a draw!";
+
     }
     else {
-        result = `You lose! ${computerChoice} beats ${humanChoice}`;
-        computerScore++;
+        computerCount++;
+        setTimeout(function () {
+            humanScore.innerText = `${humanCount}`;
+            computerScore.innerText = `${computerCount}`;
+        }, 2000)
     }
-    console.log(result);
 };
 
+// Displaying the computer choice container height
+document.addEventListener('DOMContentLoaded', () => {
+    const innerContainer = document.querySelector('.computer');
+    const innerHeight = innerContainer.offsetHeight.toFixed(2);
+    console.log(innerHeight);
+    const outerContainer = document.querySelector('.outer-comp');
+    outerContainer.style.height = `calc((${innerHeight}px - 45px) / 3)`
+});
+
+// Ui changes when a button is clicked
+playerButtons.forEach(btn => btn.addEventListener("click", () => {
+    if (selectedBtn && selectedBtn !== btn) {
+        resetBtnStyle(selectedBtn);
+    }
+    btn.style.borderColor = '#D80032';
+    selectedBtn = btn;
+}));
+
+playerIcons.forEach((icon, index) => icon.addEventListener("click", () => {
+    if (selectedIcon && selectedIcon !== icon) {
+        resetBtnStyle2(selectedIcon);
+    }
+    icon.style.color = '#D80032';
+    selectedIcon = icon;
+    humanChoice = choices[index];
+    let compChoice = getComputerChoice();
+    playRound(humanChoice, compChoice);
+    if (humanCount === 5) {
+        alert("You Won!");
+    }
+    else if (computerCount === 5) {
+        alert("You Lost!");
+    }
+}));
 
